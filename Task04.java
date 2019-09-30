e6c3fcb291041f0df261c8ff76488494db2f5516
Valeriya Sheverdina <55504182+SheverLera@users.noreply.github.com>
2019-09-30 16:29:28 +0300

    to module 3
    не понимаю почему в 4 задаче подсвечивает красным цифры

---
+++ Task04.java
@@ -0,0 +1,47 @@
+package com.rakovets.course.practice.module3;
+
+import java.util.Scanner;
+
+/**
+ * Разработать программу для desktop приложения:
+ * которая преобразует номер дня недели в его словесный аналог, если известно:
+ *
+ * @param weekDayNumber - номер дня недели
+ * @return день недели на английском языке (lower case)
+ */
+public class Task04 {
+    public static void main(String[] args) {
+        //FIXME
+        // Ввод данных осуществляется в Console, для проверки различных вариантов входных параметров
+
+        // Код необходимый для тестирования, не изменять
+        Scanner scanner = new Scanner(System.in);
+        int weekdayNumber = (args.length != 1) ? scanner.nextInt() : Integer.parseInt(args[0]);
+
+        //TODO
+        // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
+        switch ("weekDayNumber") {
+            case 1:
+                System.out.println("Понедельник");
+                break;
+            case 2:
+                System.out.println("Вторник");
+                break;
+            case 3:
+                System.out.println("Среда");
+                break;
+            case 4:
+                System.out.println("Четверг");
+                break;
+            case 5:
+                System.out.println("Пятница");
+                break;
+            case 6:
+                System.out.println("Суббота");
+                break;
+            case 7:
+                System.out.println("Воскресенье");
+                break;
+        }
+     }
+}
